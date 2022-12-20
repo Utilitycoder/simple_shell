98bc51e36f2537d8541f156e72452c3db4375283
Stanvirgo <stanvirgo7@gmail.com>
2022-12-20 11:34:30 -0800

    Updated codes

--- number_helpers.c
+++ number_helpers.c
@@ -22,6 +22,14 @@ int _atoi(char *s)
 		index++;
 	}
 
+	num = 0;
+
+	while (s[index] >= '0' && s[index] <= '9')
+	{
+		num = (s[index] - '0') + (num * 10);
+		index++;
+	}
+
 	if (neg_counter % 2 != 0)
 		num = -num;
 
